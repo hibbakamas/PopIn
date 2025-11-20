6263238bb328f710982144b723cbd0c8ae83051c
Gaby <gvega.ieu2021@student.ie.edu>
2025-11-20 21:54:01 +0100

    Edited Models

--- src/main/java/net/javaguids/popin/models/Organizer.java
+++ src/main/java/net/javaguids/popin/models/Organizer.java
@@ -3,14 +3,14 @@ package net.javaguids.popin.models;
 public class Organizer extends User {
 
     public Organizer() {
-        setRole(Role.ORGANIZER);
+        setRole(new Role("ORGANIZER"));
     }
 
     public Organizer(int id, String username, String passwordHash) {
-        super(id, username, passwordHash, Role.ORGANIZER);
+        super(id, username, passwordHash, new Role("ORGANIZER"));
     }
 
     public Organizer(String username, String passwordHash) {
-        super(username, passwordHash, Role.ORGANIZER);
+        super(username, passwordHash, new Role("ORGANIZER"));
     }
 }
